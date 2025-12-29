5600b8d1a077c779358df5a1d799d4a547916d07
toghroltp <toghroltheprogrammer@gmail.com>
2025-12-29 16:10:23 +0330

    build: add AndroidX Splash Screen dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,11 +32,12 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.core.splashscreen)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
